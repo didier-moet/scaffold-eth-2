6a5f54181adb5f819a87b260b1332bbf58e73d5c
Didier <didier.moet@gmail.com>
2024-01-09 23:07:28 -0600

    Update TxReceipt.tsx
    Less rounded, color tweaks

--- packages/nextjs/components/scaffold-eth/Contract/TxReceipt.tsx
+++ packages/nextjs/components/scaffold-eth/Contract/TxReceipt.tsx
@@ -10,7 +10,8 @@ export const TxReceipt = (
   const [txResultCopied, setTxResultCopied] = useState(false);
 
   return (
-    <div className="flex text-sm rounded-3xl peer-checked:rounded-b-none min-h-0 bg-secondary py-0">
+    
+    <div className="flex text-sm rounded peer-checked:rounded-b-none min-h-0 bg-base-100 py-0">
       <div className="mt-1 pl-2">
         {txResultCopied ? (
           <CheckCircleIcon
@@ -36,10 +37,10 @@ export const TxReceipt = (
       </div>
       <div className="flex-wrap collapse collapse-arrow">
         <input type="checkbox" className="min-h-0 peer" />
-        <div className="collapse-title text-sm min-h-0 py-1.5 pl-1">
+        <div className="collapse-title text-xs min-h-0 py-1.5 pl-1">
           <strong>Transaction Receipt</strong>
         </div>
-        <div className="collapse-content overflow-auto bg-secondary rounded-t-none rounded-3xl">
+        <div className="collapse-content overflow-auto rounded-t-none">
           <pre className="text-xs pt-4">{displayTxResult(txResult)}</pre>
         </div>
       </div>
